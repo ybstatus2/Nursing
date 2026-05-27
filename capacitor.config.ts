740cf7f4884c5077bd980c0d81120dcc60cf6310
ybstatus2 <ybstatus2@gmail.com>
2026-05-27 19:59:52 +0530

    Fix capacitor config and APK build

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.rprep.app',
-  appName: 'RPREP',
-  webDir: 'build'
-};
-
-export default config;
